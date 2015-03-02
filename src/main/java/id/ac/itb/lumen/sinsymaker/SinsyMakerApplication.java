1c4bbbfa20caadb3959427409f16195384e365ca
Hendy Irawan <ceefour666@gmail.com>
2015-03-02 13:45:37 -0430

    spring starter

---
+++ src/main/java/id/ac/itb/lumen/sinsymaker/SinsyMakerApplication.java
@@ -0,0 +1,12 @@
+package id.ac.itb.lumen.sinsymaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SinsyMakerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinsyMakerApplication.class, args);
+    }
+}
---
+++ src/test/java/id/ac/itb/lumen/sinsymaker/SinsyMakerApplicationTests.java
@@ -0,0 +1,16 @@
+package id.ac.itb.lumen.sinsymaker;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SinsyMakerApplication.class)
+public class SinsyMakerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
